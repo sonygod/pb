2be0c656fc4a45fbabaf2edefd5a81eb1e47e479
sonygod <sonygodx@gmail.com>
2019-10-24 17:35:47 +0800

    加入 20选8的sql

--- src/TestSqlite.hx
+++ src/TestSqlite.hx
@@ -267,7 +267,272 @@ class TestSqlite {
 
 		var e=" limit 100,200";
 
-		var createPerMutationQ = a + b +sum+ c + d+e; // 这个计算出开奖玩法的概率数据。
+		var createPerMutationQ = '
+		
+
+		select
+    *
+FROM
+    (
+        select
+            *,
+            n1 >= 11 as n1Big,
+            n1 < 11 as n1Small,
+            1 - n1 % 2 as n1Double,
+            n1 % 2 as n1Single,
+            n2 >= 11 as n2Big,
+            n2 < 11 as n2Small,
+            1 - n2 % 2 as n2Double,
+            n2 % 2 as n2Single,
+            n3 >= 11 as n3Big,
+            n3 < 11 as n3Small,
+            1 - n3 % 2 as n3Double,
+            n3 % 2 as n3Single,
+            n4 >= 11 as n4Big,
+            n4 < 11 as n4Small,
+            1 - n4 % 2 as n4Double,
+            n4 % 2 as n4Single,
+            n5 >= 11 as n5Big,
+            n5 < 11 as n5Small,
+            1 - n5 % 2 as n5Double,
+            n5 % 2 as n5Single,
+            n6 >= 11 as n6Big,
+            n6 < 11 as n6Small,
+            1 - n6 % 2 as n6Double,
+            n6 % 2 as n6Single,
+            n7 >= 11 as n7Big,
+            n7 < 11 as n7Small,
+            1 - n7 % 2 as n7Double,
+            n7 % 2 as n7Single,
+            n8 >= 11 as n8Big,
+            n8 < 11 as n8Small,
+            1 - n8 % 2 as n8Double,
+            n8 % 2 as n8Single,
+            CAST (
+                SUBSTR(cast(n1 as text), LENGTH(cast(n1 as text))) AS int
+            ) >= 5 AS n1TailBig,
+            CAST (
+                SUBSTR(cast(n2 as text), LENGTH(cast(n2 as text))) AS int
+            ) >= 5 AS n2TailBig,
+            CAST (
+                SUBSTR(cast(n3 as text), LENGTH(cast(n3 as text))) AS int
+            ) >= 5 AS n3TailBig,
+            CAST (
+                SUBSTR(cast(n4 as text), LENGTH(cast(n4 as text))) AS int
+            ) >= 5 AS n4TailBig,
+            CAST (
+                SUBSTR(cast(n5 as text), LENGTH(cast(n5 as text))) AS int
+            ) >= 5 AS n5TailBig,
+            CAST (
+                SUBSTR(cast(n6 as text), LENGTH(cast(n6 as text))) AS int
+            ) >= 5 AS n6TailBig,
+            CAST (
+                SUBSTR(cast(n7 as text), LENGTH(cast(n7 as text))) AS int
+            ) >= 5 AS n7TailBig,
+            CAST (
+                SUBSTR(cast(n8 as text), LENGTH(cast(n8 as text))) AS int
+            ) >= 5 AS n8TailBig,
+            CAST (
+                SUBSTR(cast(n1 as text), LENGTH(cast(n1 as text))) AS int
+            ) < 5 AS n1TailSmall,
+            CAST (
+                SUBSTR(cast(n2 as text), LENGTH(cast(n2 as text))) AS int
+            ) < 5 AS n2TailSmall,
+            CAST (
+                SUBSTR(cast(n3 as text), LENGTH(cast(n3 as text))) AS int
+            ) < 5 AS n3TailSmall,
+            CAST (
+                SUBSTR(cast(n4 as text), LENGTH(cast(n4 as text))) AS int
+            ) < 5 AS n4TailSmall,
+            CAST (
+                SUBSTR(cast(n5 as text), LENGTH(cast(n5 as text))) AS int
+            ) < 5 AS n5TailSmall,
+            CAST (
+                SUBSTR(cast(n6 as text), LENGTH(cast(n6 as text))) AS int
+            ) < 5 AS n6TailSmall,
+            CAST (
+                SUBSTR(cast(n7 as text), LENGTH(cast(n7 as text))) AS int
+            ) < 5 AS n7TailSmall,
+            CAST (
+                SUBSTR(cast(n8 as text), LENGTH(cast(n8 as text))) AS int
+            ) < 5 AS n8TailSmall,
+            ------------------------------------------------合数单双
+            1 - (
+                CAST (
+                    SUBSTR(cast(n1 as text), LENGTH(cast(n1 as text)) -1, 1) AS int
+                ) + CAST (
+                    SUBSTR(cast(n1 as text), LENGTH(cast(n1 as text)), 1) AS int
+                )
+            ) % 2 = 0 as n1CompatDouble,
+            (
+                CAST (
+                    SUBSTR(cast(n1 as text), LENGTH(cast(n1 as text)) -1, 1) AS int
+                ) + CAST (
+                    SUBSTR(cast(n1 as text), LENGTH(cast(n1 as text)), 1) AS int
+                )
+            ) % 2 = 0 as n1compatSingle,
+            1 - (
+                CAST (
+                    SUBSTR(cast(n2 as text), LENGTH(cast(n2 as text)) -1, 1) AS int
+                ) + CAST (
+                    SUBSTR(cast(n2 as text), LENGTH(cast(n2 as text)), 1) AS int
+                )
+            ) % 2 = 0 as n2CompatDouble,
+            (
+                CAST (
+                    SUBSTR(cast(n2 as text), LENGTH(cast(n2 as text)) -1, 1) AS int
+                ) + CAST (
+                    SUBSTR(cast(n2 as text), LENGTH(cast(n2 as text)), 1) AS int
+                )
+            ) % 2 = 0 as n2compatSingle,
+            1 - (
+                CAST (
+                    SUBSTR(cast(n3 as text), LENGTH(cast(n3 as text)) -1, 1) AS int
+                ) + CAST (
+                    SUBSTR(cast(n3 as text), LENGTH(cast(n3 as text)), 1) AS int
+                )
+            ) % 2 = 0 as n3CompatDouble,
+            (
+                CAST (
+                    SUBSTR(cast(n3 as text), LENGTH(cast(n3 as text)) -1, 1) AS int
+                ) + CAST (
+                    SUBSTR(cast(n3 as text), LENGTH(cast(n3 as text)), 1) AS int
+                )
+            ) % 2 = 0 as n3compatSingle,
+            1 - (
+                CAST (
+                    SUBSTR(cast(n4 as text), LENGTH(cast(n4 as text)) -1, 1) AS int
+                ) + CAST (
+                    SUBSTR(cast(n4 as text), LENGTH(cast(n4 as text)), 1) AS int
+                )
+            ) % 2 = 0 as n4CompatDouble,
+            (
+                CAST (
+                    SUBSTR(cast(n4 as text), LENGTH(cast(n4 as text)) -1, 1) AS int
+                ) + CAST (
+                    SUBSTR(cast(n4 as text), LENGTH(cast(n4 as text)), 1) AS int
+                )
+            ) % 2 = 0 as n4compatSingle,
+            1 - (
+                CAST (
+                    SUBSTR(cast(n5 as text), LENGTH(cast(n5 as text)) -1, 1) AS int
+                ) + CAST (
+                    SUBSTR(cast(n5 as text), LENGTH(cast(n5 as text)), 1) AS int
+                )
+            ) % 2 = 0 as n5CompatDouble,
+            (
+                CAST (
+                    SUBSTR(cast(n5 as text), LENGTH(cast(n5 as text)) -1, 1) AS int
+                ) + CAST (
+                    SUBSTR(cast(n5 as text), LENGTH(cast(n5 as text)), 1) AS int
+                )
+            ) % 2 = 0 as n5compatSingle,
+            1 - (
+                CAST (
+                    SUBSTR(cast(n6 as text), LENGTH(cast(n6 as text)) -1, 1) AS int
+                ) + CAST (
+                    SUBSTR(cast(n6 as text), LENGTH(cast(n6 as text)), 1) AS int
+                )
+            ) % 2 = 0 as n6CompatDouble,
+            (
+                CAST (
+                    SUBSTR(cast(n6 as text), LENGTH(cast(n6 as text)) -1, 1) AS int
+                ) + CAST (
+                    SUBSTR(cast(n6 as text), LENGTH(cast(n6 as text)), 1) AS int
+                )
+            ) % 2 = 0 as n6compatSingle,
+            1 - (
+                CAST (
+                    SUBSTR(cast(n7 as text), LENGTH(cast(n7 as text)) -1, 1) AS int
+                ) + CAST (
+                    SUBSTR(cast(n7 as text), LENGTH(cast(n7 as text)), 1) AS int
+                )
+            ) % 2 = 0 as n7CompatDouble,
+            (
+                CAST (
+                    SUBSTR(cast(n7 as text), LENGTH(cast(n7 as text)) -1, 1) AS int
+                ) + CAST (
+                    SUBSTR(cast(n7 as text), LENGTH(cast(n7 as text)), 1) AS int
+                )
+            ) % 2 = 0 as n7compatSingle,
+            1 - (
+                CAST (
+                    SUBSTR(cast(n8 as text), LENGTH(cast(n8 as text)) -1, 1) AS int
+                ) + CAST (
+                    SUBSTR(cast(n8 as text), LENGTH(cast(n8 as text)), 1) AS int
+                )
+            ) % 2 = 0 as n8CompatDouble,
+            (
+                CAST (
+                    SUBSTR(cast(n8 as text), LENGTH(cast(n8 as text)) -1, 1) AS int
+                ) + CAST (
+                    SUBSTR(cast(n8 as text), LENGTH(cast(n8 as text)), 1) AS int
+                )
+            ) % 2 = 0 as n8compatSingle
+        from(
+                SELECT
+                    --两面总和查找
+                    *,
+                    1 - sum % 2 AS sumDoble,
+                    sum % 2 AS sumSingle,
+                    sum > 84 AS sumBig,
+                    sum <= 84 AS sumSmall,
+                    CAST (SUBSTR(sumText, LENGTH(sumText)) AS int) >= 5 AS sumTailBig
+                FROM
+                    (
+                        SELECT
+                            *
+                        FROM
+                            (
+                                SELECT
+                                    E1.i AS n1,
+                                    E2.i AS n2,
+                                    E3.i AS n3,
+                                    E4.i AS n4,
+                                    E5.i AS n5,
+                                    E6.i AS n6,
+                                    E7.i AS n7,
+                                    E8.i AS n8,
+                                    (
+                                        E1.i + E2.i + + E3.i + + E4.i + + E5.i + + E6.i + + E7.i + E8.i
+                                    ) AS sum,
+                                    CAST (
+                                        (
+                                            E1.i + E2.i + + E3.i + + E4.i + + E5.i + + E6.i + + E7.i + E8.i
+                                        ) AS text
+                                    ) AS sumText
+                                FROM
+                                    Elements_GDKLSF AS E1,
+                                    Elements_GDKLSF AS E2,
+                                    Elements_GDKLSF AS E3,
+                                    Elements_GDKLSF AS E4,
+                                    Elements_GDKLSF AS E5,
+                                    Elements_GDKLSF AS E6,
+                                    Elements_GDKLSF AS E7,
+                                    Elements_GDKLSF AS E8
+                                WHERE
+                                    E2.i NOT IN (E1.i)
+                                    AND E3.i NOT IN (E1.i, E2.i)
+                                    AND E4.i NOT IN (E1.i, E2.i, E3.i)
+                                    AND E5.i NOT IN (E1.i, E2.i, E3.i, E4.i)
+                                    AND E6.i NOT IN (E1.i, E2.i, E3.i, E4.i, E5.i)
+                                    AND E7.i NOT IN (E1.i, E2.i, E3.i, E4.i, E5.i, E6.i)
+                                    AND E8.i NOT IN (
+                                        E1.i,
+                                        E2.i,
+                                        E3.i,
+                                        E4.i,
+                                        E5.i,
+                                        E6.i,
+                                        E7.i
+                                    )
+                            )
+                    )
+            )
+    )
+
+	limit 10
+		';
 
 		trace(createPerMutationQ);
 
