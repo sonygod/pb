a5f5f4d12ea017eb8b5a19e5a86e8b671cb166b1
sonygod <sonygodx@gmail.com>
2020-02-29 23:56:23 +0800

    111

--- src/gen/GenKLSFBytes.hx
+++ src/gen/GenKLSFBytes.hx
@@ -123,6 +123,8 @@ class GenKLSFBytes {
 				var itemBytes = Bytes.alloc(8);
 				var from = i * eachCount;
 				var to = (i + 1) * eachCount;
+
+				trace('from =$from  to=$to');
 				for (k in from...to) {
 					var currentIndex = k;
 
@@ -143,7 +145,12 @@ class GenKLSFBytes {
 						trace(index);
 					}
 
-					if (index == totals) {
+					 if(index>totals-100){
+						 trace(index);
+					 }
+
+
+					if (index == totals-1) {
 						trace("恭喜你，完成了");
 					}
 				}
